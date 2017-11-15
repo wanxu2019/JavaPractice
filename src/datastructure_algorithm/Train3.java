1c429d8c262e845a0be7b5223ff6b76471545b77
wanxu <1334793794@qq.com>
2017-11-16 07:15:50 +0800

    Add Train3

---
+++ src/datastructure_algorithm/Train3.java
@@ -0,0 +1,14 @@
+package datastructure_algorithm;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by Json Wan on 2017/11/16.
+ * Description:
+ */
+public class Train3 {
+    public static void main(String[] args) {
+        Collection<Integer> integers=new ArrayList<>();
+    }
+}
--- src/reflection/Quiz2.java
+++ src/reflection/Quiz2.java
@@ -4,6 +4,9 @@ package reflection;
  * 开发一个对象工厂池，这个对象工厂池不仅可以管理对象的String类型成员变量的值，还可以管理容器中对象的其他类型成员变量的值，甚至可以将对象的成员变量设置成引用到容器中其他对象（这就是Spring所提出的控制反转，即IoC）。
  * Created by Json Wan on 2017/10/17.
  */
+class InstanceFactory{
+
+}
 public class Quiz2 {
 
 }
